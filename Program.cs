3f6a282ea5a09aee8514735d0d4feb31f00d0285
Che Sergiu <37384588+CHESERGIU@users.noreply.github.com>
2020-07-03 10:52:07 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace App24
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input;
+            int prod = 1;
+            int count = 0;
+            do
+            {
+                input = Console.ReadLine();
+                if (input != "x")
+                {
+                    count++;
+                    int value = int.Parse(input);
+                    for (int i = 0; i < count; i++)
+                    {
+                        prod *= value;
+                        count--;
+                    }
+                }
+                else
+                {
+                    break;
+                }
+            } while (!(input == "x"));
+            Console.WriteLine(prod);
+        }
+    }
+}
